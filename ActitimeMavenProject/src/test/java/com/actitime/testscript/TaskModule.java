d52d214cec3f4b9a4fdc651bf89366e3122344e0
Dell Inspiron 15 <Dell Inspiron 15@DESKTOP-KEFG07B>
2023-08-04 00:28:19 +0530

    Maven_project_first_commit

---
+++ ActitimeMavenProject/src/test/java/com/actitime/testscript/TaskModule.java
@@ -0,0 +1,11 @@
+package com.actitime.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TaskModule {
+	@Test
+	public void CreateTask() {
+		Reporter.log("Create task from maven",true);
+	}
+}
